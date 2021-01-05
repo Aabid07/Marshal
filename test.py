0a67b9c50b064945df69c0814f5eca43cac40920
Aabid07 <aabidhussain.inc@outlook.com>
2021-01-05 15:37:33 +0530

    added function to test.py file

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
 import re
 import os
 
-print("Hello World!")
-print("Let's try to learn GIT in details")
+def print_the_output():
+    print("Hello World!")
+    print("Let's try to learn GIT in details")
+    return 0
+
+
+def print_the_output_Once_More():
+    if not print_the_output():
+        print("Called function named: print_the_output() with return 0")
+
 
 
