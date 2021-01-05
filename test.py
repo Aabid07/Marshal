57ec41e09ac518254ecf2d72d33f758fbebbb0fd
Aabid <aabidhussain.inc@outlook.com>
2021-01-05 16:02:43 +0530

    this is new changes.

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
 import re
 import os
 
-print("Hello World!")
-print("Let's try to learn GIT in details")
+
+def function_to_print():
+    print("Hello World!")
+    print("Let's try to learn GIT in details")
+    return 0
+
+
+def function_to_verify():
+    if not function_to_print():
+        print("This test has pass!!")
+
+
+if __name__ == "__main__":
+    function_to_verify()
