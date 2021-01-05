70d3c3b9cde7b32b28174311d6ef5656a26e0ad8
Aabid <aabidhussain.inc@outlook.com>
2021-01-05 15:54:07 +0530

    pull request

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
 import re
 import os
 
-def print_the_output():
-    print("Hello World!")
-    print("Let's try to learn GIT in details")
-    return 0
-
-
-def print_the_output_Once_More():
-    if not print_the_output():
-        print("Called function named: print_the_output() with return 0")
-
-
-
+print("Hello World!")
+print("Let's try to learn GIT in details")
