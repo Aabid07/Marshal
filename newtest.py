5de51d8d3087b54c1248637870646e0d647e8e7c
Aabid07 <aabidhussain.inc@outlook.com>
2021-01-06 17:07:25 +0530

    Added user defined exception

--- newtest.py
+++ newtest.py
@@ -4,3 +4,17 @@ __doc__ = """ This is new doc file and it is used to
           """
 
 __author__ = "Mohammad Aabid Hussain"
+
+
+class UserException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+        # super(UserException, self).__init__()
+        self.message()
+
+    def message(self):
+        return Exception(f"ERROR: {self.msg}")
+
+
+EX = UserException("This is userException Class")
+print(EX)
